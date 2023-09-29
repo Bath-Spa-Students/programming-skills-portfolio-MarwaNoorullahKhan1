5e69f677f0015a45dac52fff23822823f93a710e
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-09-29 12:53:29 +0400

    exercise3.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 1.py/Exercise3.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 1.py/Exercise3.py
@@ -1,11 +1,9 @@
-print("Hello world")
-a, b, c=5, 6, 7
-print(a)
-print(b)
-print(c)
-a=("My name is Marwa")
-b=("Im currently studying in Bathspa university")
-c=("i love my major(CC)")
-print(a)
-print(b)
-print(c)
+from datetime import datetime 
+#datetime object containing current date and time
+now = datetime.now()
+print("now =", now)
+#dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
+
+
