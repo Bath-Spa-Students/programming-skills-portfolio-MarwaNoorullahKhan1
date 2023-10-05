1fc459d2948996cb1565799f63b5ef4f8c2ede60
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-10-05 16:31:55 +0400

    checking  python version

--- Chapter 1- Getting Started with Python/Exercises/exercise 1.py/exercise2.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 1.py/exercise2.py
@@ -1,4 +1,8 @@
-import sys
-print(3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)])
 import platform
-print(platform.(3.10.11))
+print(platform.python_version())
+
+
+
+
+
+
