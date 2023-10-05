e161a3b22c5e59c70e5eaca7303a7ec61d3e9052
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-10-05 17:00:10 +0400

    update exercise5.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 1.py/Exercise5.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 1.py/Exercise5.py
@@ -1,5 +1,5 @@
+
 from math import pi
-r= float(input("input the radius of the circles :"))
-print ("The area of the circle with radius "+str(r) + " is:" + str(pi * r**2))
-input("input the radius of the circle :1.1")
-input ("input the area of the circle with the radius 1.1 is: 3.8013271108436504")
+r = float(input ("Input the radius of the circle : "))
+calculateArea = str(pi * r**2);
+print ("The area of the circle with radius " + str(r) + " is: " + calculateArea)
