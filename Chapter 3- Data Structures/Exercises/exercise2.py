2b77f890d781b4e0a2ff7787d789719efb89703f
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-10-23 17:35:31 +0400

    exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,4 @@
+names = ["Loisa" , "Kairi" , "Rinoa" , "Pheobe"]
+print("Hello, bestie , {}".format(names[0]))
+print("Hello, bestie , {}".format(names[1]))
+print("Hello, bestie , {}".format(names[2]))
