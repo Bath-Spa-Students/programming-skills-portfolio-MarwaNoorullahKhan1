430e119880dc2293433113b78f89dc2e0b7d30f7
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-10-23 17:46:33 +0400

    exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,5 @@
+favorite_transportation = ["Car.", "Metro." , "Bike."]
+
+print("I usually go out using {}".format(favorite_transportation[0]))
+print("And I like going to different places using {}".format(favorite_transportation[1]))
+print("And also I really like going out on rainy days using {}".format(favorite_transportation[2]))
