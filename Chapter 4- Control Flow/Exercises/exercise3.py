7c627ff4a9a50aa49d1066138d60bca339b7576b
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-10-26 19:13:06 +0400

    chp4exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -0,0 +1,11 @@
+alien_color = "red"
+if alien_color == "green":
+    print("you have earned 5 points")
+elif alien_color =="yellow":
+    print("you have earned 10 points")
+else:
+    print("you just earned 15 points")
+    
+
+
+
