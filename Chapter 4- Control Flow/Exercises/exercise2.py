f0492fbd6a657a55323ab7e0bf4ddd4e1962bb6a
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-10-26 17:46:16 +0400

    Update exercise2.py

--- Chapter 4- Control Flow/Exercises/exercise2.py
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -1,8 +1,16 @@
-alien_color = "red"
+#if chain :
+alien_color = "green"
 if alien_color =="green":
     print("you earned 5 points")
-elif alien_color =="yellow": #elif= else if
-    print("you have earned 10 points")
 else:
-    print("you have earned 15 points")
+    print("you have earned 10 points")
 
+
+
+#else chain:
+alien_color = "yellow"
+if alien_color =="green":
+    print("you have earned 5 points")
+else:
+    print("ypu have earned 10 points")
+    
