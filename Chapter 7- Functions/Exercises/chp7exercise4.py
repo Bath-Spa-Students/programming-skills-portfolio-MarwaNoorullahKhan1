e8fbe1efc8d6e28b31d00ff4bfa85f035ea74bd6
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-11-02 21:43:32 +0400

    chp7exercise4.py

---
+++ Chapter 7- Functions/Exercises/chp7exercise4.py
@@ -0,0 +1,6 @@
+def make_shirt(size = 'large',message='  Im not a fan of coding  '):
+	print(f"The size of T-shirt is {size}.")
+	print(f"I would like it printed as {message.title()} in T-shirt.")
+
+make_shirt()
+make_shirt(message='  Mom: learn it ;/  ',size ='medium')
