b674dfec92213926648f0a8e0dbc95b6bbffbd74
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-11-02 21:34:24 +0400

    chp7exercise3.py

---
+++ Chapter 7- Functions/Exercises/chp7exercise3.py
@@ -0,0 +1,6 @@
+def make_shirt(size,message):
+	print(f"The size of T-shirt is {size}.")
+	print(f"I would like it printed as {message.title()} in T-shirt.")
+
+make_shirt(15," are you serious ")
+make_shirt(message = '  Dont be so serious  ',size =20)
