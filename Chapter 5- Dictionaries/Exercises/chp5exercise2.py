41a97725076a137ddffbcb82800b139684731c48
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-11-02 17:20:20 +0400

    chp5exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/chp5exercise2.py
@@ -0,0 +1,22 @@
+favorite_numbers = {
+    "kairi": 67,
+    "rinoa": 98,
+    "loisa": 1,
+    "pheobe": 87,
+    "me": 2,
+    }
+
+num = favorite_numbers["kairi"]
+print("kairi's favorite number is " + str(num) + ".")
+
+num = favorite_numbers["rinoa"]
+print("rinoa's favorite number is " + str(num) + ".")
+
+num = favorite_numbers["loisa"]
+print("loisa's favorite number is " + str(num) + ".")
+
+num = favorite_numbers["pheobe"]
+print("pheobe's favorite number is " + str(num) + ".")
+
+num = favorite_numbers["me"]
+print("me's favorite number is " + str(num) + ".")
