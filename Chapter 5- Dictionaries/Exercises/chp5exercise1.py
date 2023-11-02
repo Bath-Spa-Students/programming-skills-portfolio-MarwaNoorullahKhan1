6cf148f9183cc33f34260b0e5bac2aa4420b80fe
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-11-02 17:08:44 +0400

    chp5exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/chp5exercise1.py
@@ -0,0 +1,10 @@
+person = {
+    "first_name": "Badr",
+    "last_name" : "Aboud",
+    "age" : 23,
+    "city" : "Ras al khaimah"
+    }
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
