3a16582acefc39de81848d0cef8f292fdecb0b8f
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-11-02 20:30:18 +0400

    chp6exercise2.py

---
+++ Chapter 6- Loops/Exercises/chp6exercise2.py
@@ -0,0 +1,12 @@
+while True:
+    age = input("How old are you? ")
+    
+    if age == "quit":
+        break
+    age = int(age)
+    if age < 3:
+        print("Your ticket is free")
+    elif age < 12:
+        print("Your ticket is $10") 
+    else:
+        print("Your ticket is $15")
