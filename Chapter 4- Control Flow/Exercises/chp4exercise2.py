06f4a3181002839039f2136c73def047f9640eb2
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-11-02 21:55:24 +0400

    chp4exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/chp4exercise2.py
@@ -0,0 +1,15 @@
+#if chain :
+alien_color = "green"
+if alien_color =="green":
+    print("you earned 5 points")
+else:
+    print("you have earned 10 points")
+
+
+
+#else chain:
+alien_color = "yellow"
+if alien_color =="green":
+    print("you have earned 5 points")
+else:
+    print("ypu have earned 10 points")
