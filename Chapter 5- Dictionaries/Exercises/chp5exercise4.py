d8f28bf58a709b777ad0de5f5fdd50a043ce10b0
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-11-02 17:44:44 +0400

    chp5exercise4.py

---
+++ Chapter 5- Dictionaries/Exercises/chp5exercise4.py
@@ -0,0 +1,18 @@
+rivers = {
+    "Nile": "Egypt",
+    "Mississippi": "United states",
+    "Amazon": "South america",
+    "Missouri": "North america",
+    "Yangtze": "China",
+    }
+
+for river, country in rivers.items():
+    print("The " + river.title() + " flows through " + country.title() + ".")
+
+print("\nThe following rivers are included in this data set:")
+for river in rivers.keys():
+    print("- " + river.title())
+
+print("\nThe following countries are included in this data set:")
+for country in rivers.values():
+    print("- " + country.title())
