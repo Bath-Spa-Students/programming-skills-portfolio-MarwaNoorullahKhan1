309d4ce8f0843e9722c646bc9f1d0542229895ae
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-11-02 21:47:10 +0400

    chp7exercise5.py

--- Chapter 7- Functions/Exercises/chp7exercise2.py
+++ Chapter 7- Functions/Exercises/chp7exercise2.py
@@ -2,3 +2,4 @@ def favorite_book(title):
 	print(f"One of my favorite books is {title.title()}.\n")
   
 favorite_book("The Boy, the Mole, the Fox and the Horse: by Chrlie Mackesy ")
+
---
+++ Chapter 7- Functions/Exercises/chp7exercise5.py
@@ -0,0 +1,6 @@
+def describe_city(city_name='ShangHai',country = 'China'):
+	print(f"{city_name.title()} is in {country.title()}.")
+
+describe_city()
+describe_city('Tokyo','Japan')
+describe_city('Dubai','United Arab Emirates')
