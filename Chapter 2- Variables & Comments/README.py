4b76cb68d7d9a11499cf1279974a229a5b090101
MarwaNoorullahKhan1 <144215836+MarwaNoorullahKhan1@users.noreply.github.com>
2023-11-10 11:58:26 +0400

    README.py

--- Chapter 2- Variables & Comments/README.py
+++ Chapter 2- Variables & Comments/README.py
@@ -1,43 +1,71 @@
-# Variable Reassignment
-print ("Name", " Marwa " ,98720)
-print  ( "Father's Name" , ": Noorullah Gullkhan" , 12376)
-print ("University" , " Bathyspa" , 456788)
+# Chapter 2 Exercises
 
-# Data types int , float , str
-name = "Marwa"
-name = "Bathspa"
+Exercises with a tick mark :ballot_box_with_check: represent exercises that must be submitted as part of your Programming Skills Portfolio as a minimum expectation. Completing more exercises provides the opportunity to attain higher marks. For each exercise you should create a _**new project**_ with the name of the exercise and save it to this exercises folder in your local repository.
 
-print (name)
+Once you have completed your solution you should make sure you commit and push your solutions to your remote repository on GitHub. You can commit and push as many changes to your solutions as you wish, only those pushed before the chapter deadlines will be marked for the Programming Skills Portfolio.  
 
-# By default / sign gives floating numbers //it gives integer
-print (13/2)
-print (13//2)
 
-# How to take input from user By default it takes string from user
-name =input("Enter Name: ")
-print = input ("Enter University Name: ")
-print (name)
+&nbsp;
 
-#user age ( By default it takes string from user) we need type casting
-'''age = input (" Enter your age : ")
-print (age)'''
+## Exercise 1: Variables:ballot_box_with_check:
 
-# Type Casting
-age = int (input ("Enter your age: "))
-print (age)
 
-# Math Operators
-print ((2*3/8+(23+5)))
+Assign a message to a variable, and print that message.
+Then change the value of the variable to a new message, and print the new
+message.
 
-#Type cast float to int
-ans = ((2*3/8+(23+5)))
-print (int (ans))
+&nbsp;
+&nbsp;
+&nbsp;
+## Exercise 2: Variables :ballot_box_with_check:
 
-# To write formulas in more than one line 
-ans = ((2*3/8+\
-        (23+5)))
-print (int (ans))
+Find a quote from a famous person you admire. Print the quote and the name of its author. 
 
-#String Concatenation with, and +
-print (" My" , " Name" , "Marwa")
-print("My " + "Name" + "is" + "Marwa")
+Your output should look something like the following, including the quotation marks:
+
+Albert Einstein once said, “A person who never made a mistake never tried anything new.
+
+
+&nbsp;
+&nbsp;
+&nbsp;
+## Exercise 3: Stripping Names :ballot_box_with_check:
+
+Tidy up the code to make it easier to understand
+
+Use a variable to represent a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, “\t” and “\n”, at least once.
+
+Print the name once, so the whitespace around the name is displayed. 
+
+Then print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().
+
+
+&nbsp;
+&nbsp;
+&nbsp;
+## Exercise 4: Favorite Number :ballot_box_with_check:
+Use a variable to represent your favorite number. Then,using that variable, create a message that reveals your favorite number. Print
+that message.
+
+&nbsp;
+&nbsp;
+&nbsp;
+
+## Exercise 5: USB Shopper :ballot_box_with_check:
+
+A girl heads to a computer shop to buy some USB sticks. She loves USB sticks and wants as many as she can get for £50. They are £6 each.
+
+Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
+
+You will to use the arithmetic operators to complete this exercise.
+
+
+
+
+
+
+
+
+&nbsp;
+&nbsp;
+&nbsp;
